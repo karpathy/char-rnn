c2cabb5583eb9fca61d6f9190208710500402d24
Ajay Talati <ajaytalati@googlemail.com>
2015-05-29 00:18:20 +0100

    Cleaned clone_many_times(net, T)
    `parametersNoGrad` and `net.parameters` were not needed

--- util/model_utils.lua
+++ util/model_utils.lua
@@ -108,55 +108,27 @@ function model_utils.combine_all_parameters(...)
 end
 
 
-
-
 function model_utils.clone_many_times(net, T)
-    local clones = {}
-
-    local params, gradParams
-    if net.parameters then
-        params, gradParams = net:parameters()
-        if params == nil then
-            params = {}
-        end
+  local clones = {}
+  local params, gradParams = net:parameters()
+  local mem = torch.MemoryFile("w"):binary()
+  mem:writeObject(net)
+  for t = 1, T do
+    -- We need to use a new reader for each clone.
+    -- We don't want to use the pointers to already read objects.
+    local reader = torch.MemoryFile(mem:storage(), "r"):binary()
+    local clone = reader:readObject()
+    reader:close()
+    local cloneParams, cloneGradParams = clone:parameters()
+    for i = 1, #params do
+      cloneParams[i]:set(params[i])
+      cloneGradParams[i]:set(gradParams[i])
     end
-
-    local paramsNoGrad
-    if net.parametersNoGrad then
-        paramsNoGrad = net:parametersNoGrad()
-    end
-
-    local mem = torch.MemoryFile("w"):binary()
-    mem:writeObject(net)
-
-    for t = 1, T do
-        -- We need to use a new reader for each clone.
-        -- We don't want to use the pointers to already read objects.
-        local reader = torch.MemoryFile(mem:storage(), "r"):binary()
-        local clone = reader:readObject()
-        reader:close()
-
-        if net.parameters then
-            local cloneParams, cloneGradParams = clone:parameters()
-            local cloneParamsNoGrad
-            for i = 1, #params do
-                cloneParams[i]:set(params[i])
-                cloneGradParams[i]:set(gradParams[i])
-            end
-            if paramsNoGrad then
-                cloneParamsNoGrad = clone:parametersNoGrad()
-                for i =1,#paramsNoGrad do
-                    cloneParamsNoGrad[i]:set(paramsNoGrad[i])
-                end
-            end
-        end
-
-        clones[t] = clone
-        collectgarbage()
-    end
-
-    mem:close()
-    return clones
+    clones[t] = clone
+    collectgarbage()
+  end
+  mem:close()
+  return clones
 end
 
 return model_utils
