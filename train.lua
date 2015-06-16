8140f9bdb8aee7a932ad31affbce86330b18368d
Bret Mattingly II <bomattin@users.noreply.github.com>
2015-06-16 09:19:44 -0400

    Update train.lua

--- train.lua
+++ train.lua
@@ -71,13 +71,15 @@ local split_sizes = {opt.train_frac, opt.val_frac, test_frac}
 if opt.gpuid >= 0 then
     local ok, cunn = pcall(require, 'cunn')
     local ok2, cutorch = pcall(require, 'cutorch')
-    if not ok then print('package cunn not found!') end
+    if not ok then print('package cunn not found!) end
     if not ok2 then print('package cutorch not found!') end
     if ok and ok2 then
         print('using CUDA on GPU ' .. opt.gpuid .. '...')
         cutorch.setDevice(opt.gpuid + 1) -- note +1 to make it 0 indexed! sigh lua
         cutorch.manualSeed(opt.seed)
     else
+        print('If cutorch and cunn are installed, your CUDA toolkit may be improperly configured.')
+        print('Check your CUDA toolkit installation, rebuild cutorch and cunn, and try again.')
         print('Falling back on CPU mode')
         opt.gpuid = -1 -- overwrite user setting
     end
