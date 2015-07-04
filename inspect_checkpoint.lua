6158d5f8201bc1f05e8e3030504a0d91923255ca
Hugh Perkins <hughperkins@gmail.com>
2015-07-04 22:42:26 +0800

    add -opencl 1 to inspect_checkpoint.lua

--- inspect_checkpoint.lua
+++ inspect_checkpoint.lua
@@ -14,16 +14,24 @@ cmd:text()
 cmd:text('Options')
 cmd:argument('-model','model to load')
 cmd:option('-gpuid',0,'gpu to use')
+cmd:option('-opencl',0,'use OpenCL (instead of CUDA)')
 cmd:text()
 
 -- parse input params
 opt = cmd:parse(arg)
 
-if opt.gpuid >= 0 then
+if opt.gpuid >= 0 and opt.opencl == 0 then
     print('using CUDA on GPU ' .. opt.gpuid .. '...')
     require 'cutorch'
     require 'cunn'
     cutorch.setDevice(opt.gpuid + 1)
+end
+
+if opt.gpuid >= 0 and opt.opencl == 1 then
+    print('using OpenCL on GPU ' .. opt.gpuid .. '...')
+    require 'cltorch'
+    require 'clnn'
+    cltorch.setDevice(opt.gpuid + 1)
 end
 
 local model = torch.load(opt.model)
